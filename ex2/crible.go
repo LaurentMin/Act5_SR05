8006312cdc94362daeaf10bac804311367740d09
Laurent MINATCHY <laurent.minatchy@etu.utc.fr>
2024-04-12 21:40:47 +0200

    defer wg.Done()

--- ex2/crible.go
+++ ex2/crible.go
@@ -9,7 +9,7 @@ import (
 )
 
 func filtrer(i int, c chan int, resultChan chan int, wg *sync.WaitGroup) {
-	
+	defer wg.Done() // Marquer la fin de la goroutine lorsque la fonction se termine
 	var flag bool = false // Flag pour indiquer si une nouvelle goroutine a été créée
 	cbis := make(chan int) // Canal pour la prochaine goroutine
 
@@ -41,7 +41,6 @@ func filtrer(i int, c chan int, resultChan chan int, wg *sync.WaitGroup) {
 			fmt.Println("goroutine ", i, " filtre ", n)
 		}
 	}
-	wg.Done() // Marquer la fin de la goroutine
 }
 
 func main() {
